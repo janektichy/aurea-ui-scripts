564d2334827006044d703aeeb57aa116eefbdc46
janektichy <88430495+janektichy@users.noreply.github.com>
2024-11-03 12:48:34 +0100

    Update FAQ-view-scripts.js

--- FAQ-view-scripts.js
+++ FAQ-view-scripts.js
@@ -1,7 +1,10 @@
-setTimeout(function() {
+$(document).ready(function() {
+    console.log("Document is ready");
+
     if ($("#testProp").length) {
+        console.log("#testProp found, adding class");
         $("#testProp").addClass("scriptClass");
     } else {
-        console.log("#testProp element not found");
+        console.log("#testProp not found at document ready");
     }
-}, 2000); // 2-second delay
+});
